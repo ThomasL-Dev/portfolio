41bad56b4ef0b28524365816360f21816b4e6c59
Thomas <thomaslmails@gmail.com>
2019-11-30 04:40:48 +0100

    first commit of portfolio

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="bandeau">
+		<div id="title-bandeau">
+			<h1>titre</h1>
+		</div>
+		<div id="btn-bandeau">
+			<input type="submit" value="Acceuil">
+			<input type="submit" value="Développement">
+			<input type="submit" value="Contact">
+		</div>
+		<div id="res-btn-bandeau">
+			<input type="submit" value="linkdin" id="res">
+			<input type="submit" value="github" id="res">
+		</div>
+	</div>	
+
+	<div id="main">
+		<div id="start">
+			<img id="start-img" src="/img/start.jpg">
+		</div>	
+		<div id="presentation">
+			<h1>Presentation</h1>
+		</div>	
+		<div id="programmation">
+			programmation
+		</div>	
+	</div>
+</body>
+</html>
