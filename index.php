a092f48a0bfc507f823fb759743cde31963b2349
thomaslmails@gmail.com <thomaslmails@gmail.com>
2019-11-30 17:51:17 +0100

    modif & ajout

--- index.php
+++ index.php
@@ -3,31 +3,50 @@
 <head>
 	<title>Portfolio</title>
 	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="icon" href="img/icon3.png" />
 </head>
 <body>
 	<div class="bandeau">
-		<img src="img/t.jpg" id="img-bandeau">
+
 		<div id="title-bandeau">
 			<h1>titre</h1>
 		</div>
+
 		<div id="btn-bandeau">
-			<input type="submit" value="Acceuil">
-			<input type="submit" value="Développement">
-			<input type="submit" value="Contact">
+			<input type="submit" value="Acceuil" onclick="window.location='/index.php';">
+			<input type="submit" value="Développement" onclick="window.location='/dev.php';">
+			<input type="submit" value="Contact" onclick="window.location='/contact.php';">
 		</div>
+		
 		<div id="res-btn-bandeau">
-			<input type="submit" value="linkdin" id="res">
-			<input type="submit" value="github" id="res">
+			<a href="https://github.com/ThomasL-Dev/">
+				<img id="github-img" src="/img/github.png"/>
+			</a>
+			
+			<a href="https://www.linkedin.com/in/thomas-legay-b5b634171/">
+				<img id="linkdin-img" src="/img/linkdin.png"/>
+			</a>
 		</div>
+
+		<div id="div-img-bandeau">
+			<img src="img/t.jpg" id="img-bandeau">
+		</div>
+		
 	</div>	
 
 	<div id="main">
+		
 		<div id="start">
 			<img id="start-img" src="/img/start.jpg">
+				<a href="#presentation">
+					<img id="goto-presentation-img" src="/img/icon1.png"/>
+				</a>
 		</div>	
+		
 		<div id="presentation">
 			<h1>Presentation</h1>
 		</div>	
+		
 		<div id="programmation">
 			programmation
 		</div>	
