a18eacc994eafefd591d18565c3b466e6571c36e
thomaslmails@gmail.com <thomaslmails@gmail.com>
2019-12-01 04:26:34 +0100

    liste des jeux

---
+++ gamelist.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Secret Games</title>
+	<link rel="stylesheet" type="text/css" href="css/gamelist.css">
+	<link rel="icon" href="img/icon3.png" />
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/8bit-wonder" type="text/css"/> 
+</head>
+<body>
+
+	<div id="main">
+
+		<h1>>JEUX</h1>
+
+		<div id="gamelist">
+			
+			<h3 id="game1"> <a href="snake.html"> >Snake </a></h3>
+
+			<h3 id="game2">  <a href="breakout.html"> >Breakout </a></h3>
+
+			<h3 id="game3">  <a href="tetris.html"> >Tetris </a></h3>
+
+			<h3 id="game4">  <a href="pong.html"> >Pong </a></h3>
+		
+		</div>
+
+	</div>
+
+		<img id="bg" src="/img/gl.jpg">
+
+</body>
+</html>
