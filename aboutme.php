aa29d6c1d71309e3354ce7f7cb36d0cdd9f725d3
thomaslmails@gmail.com <thomaslmails@gmail.com>
2019-12-01 03:08:10 +0100

    graphics fini & add des jeux

---
+++ aboutme.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="icon" href="img/icon3.png" />
+</head>
+<body>
+	<div class="bandeau">
+
+		<div id="title-bandeau">
+			<h1>LEGAY THOMAS</h1>
+			<img src="img/ul.png" id="underline">
+		</div>
+
+		<div id="div-btn-bandeau">
+			<input type="submit" id="btn-bandeau" value="Acceuil" onclick="window.location='/index.php';">
+			<input type="submit" id="btn-bandeau" value="Développement" onclick="window.location='/dev.php';">
+			<input type="submit" id="btn-bandeau" value="à Propos" onclick="window.location='/aboutme.php';">
+		</div>
+		
+		<div id="res-btn-bandeau">
+			<a href="https://github.com/ThomasL-Dev/">
+				<img id="github-img" src="/img/github.png"/>
+			</a>
+			
+			<a href="https://www.linkedin.com/in/thomas-legay-b5b634171/">
+				<img id="linkdin-img" src="/img/linkdin.png"/>
+			</a>
+		</div>
+
+		<div id="footer">
+			Copyright © 2019 - Legay Thomas
+		</div>
+
+		<div id="div-img-bandeau">
+			<img src="img/t.jpg" id="img-bandeau">
+		</div>
+		
+	</div>	
+
+
+	<div id="main">
+
+		<a href="snake.html">
+			<img id="gb-img" src="/img/gb.png"/>
+		</a>
+
+
+		<img id="pp" src="/img/p.png">
+
+
+		<div id="infos-perso">
+			
+			<h1 id="infos-title">Informations</h1>
+			
+			<div id="logo-infos-perso">
+				<img id="img-infos" src="/img/anniv.png">
+				<img id="img-infos" src="/img/localisation.png">
+				<img id="img-infos" src="/img/mail.png">
+				<img id="img-infos" src="/img/pdf.png">
+			</div>
+		
+			<div id="text-infos-perso">	
+				<h2 id="text-infos">20 ans</h2>
+				<h2 id="text-infos">03000, Moulins</h2>
+				<h2 id="text-infos"><a id="mail" href="mailto:thomaslmails@gmail.com">thomaslmails@gmail.com</a></h2>
+				<h2 id="text-infos"><a id="cv" href="img/CV - LEGAY Thomas.pdf">CV</a></h2>
+			</div>
+		</div>
+
+		<div id="description">
+			<h1 id="description-title">Description</h1>
+
+			<p id="description-text1">
+				J'ai 20 ans, je touche à tout en ce qui concerne les ordinateurs depuis tout jeune, même si je suis plus passionné par le Développement Web/Logiciels, cela permet de faire ressortir ma créativité et surtout de rendre la vie beaucoup plus simple.
+			</p>
+			<p id="description-text2">
+				Je peux intervenir sur tout type de projet en Python, c'est le langage que je maîtrise le mieux, je suis actuellement entrain de développer un assistant personnel pour optimiser ma vie tout en Python.
+			</p>
+			<p id="description-text3">
+				Je maîtrise tout le reste avec modération donc si des expériences peuvent m'en apprendre plus je suis preneur.
+			</p>
+
+		</div>
+
+		<img src="img/wpam2.jpg" id="bgam">
+
+	</div>	
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 <html>
 <head>
 	<title>Portfolio</title>
-	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 	<link rel="icon" href="img/icon3.png" />
 </head>
 <body>
 	<div class="bandeau">
 
 		<div id="title-bandeau">
-			<h1>LEGAY Thomas</h1>
+			<h1>LEGAY THOMAS</h1>
+			<img src="img/ul.png" id="underline">
 		</div>
 
 		<div id="div-btn-bandeau">
 			<input type="submit" id="btn-bandeau" value="Acceuil" onclick="window.location='/index.php';">
 			<input type="submit" id="btn-bandeau" value="Développement" onclick="window.location='/dev.php';">
-			<input type="submit" id="btn-bandeau" value="Contact" onclick="window.location='/contact.php';">
+			<input type="submit" id="btn-bandeau" value="à Propos" onclick="window.location='/aboutme.php';">
 		</div>
 		
 		<div id="res-btn-bandeau">
@@ -28,13 +29,25 @@
 			</a>
 		</div>
 
+		<div id="footer">
+			Copyright © 2019 - Legay Thomas
+		</div>
+
 		<div id="div-img-bandeau">
 			<img src="img/t.jpg" id="img-bandeau">
 		</div>
 		
 	</div>	
 
+
+
+
+
 	<div id="main">
+
+		<a href="snake.html">
+			<img id="gb-img" src="/img/gb.png"/>
+		</a>
 		
 		<div id="start">
 			<img id="start-img" src="/img/start1.jpg">
@@ -47,20 +60,73 @@
 		</div>	
 		
 		<div id="presentation">
-			<img id="start-img" src="/img/t2.jpg">
-			<h1>Presentation</h1>
-			
-			<p>J'ai 20 ans, je touche à tout en ce qui concerne les ordinateurs depuis tout jeune, même si je suis plus passionné par le Développement Web/Logiciels, cela permet de faire ressortir ma créativité et surtout de rendre la vie beaucoup plus simple.
 
-			Je peux intervenir sur tout type de projet en Python, c'est le langage que je maîtrise le mieux, je suis actuellement entrain de développer un assistant personnel pour optimiser ma vie tout en Python.
+			<div id="presentation-text">
+				<h1 id="presentation-title"><u>Compétences</u></h1>
+			</div>
 
-			Je maîtrise tout le reste avec modération donc si des expériences peuvent m'en apprendre plus je suis preneur.
-			</p>
+			<div id="div-img-competence">
+				
+				<div id="compet1">
+					<p>python</p>
+					<img id="img-competence" src="/img/python.png">
+				</div>
+
+				<div id="compet2">
+					<p>java/android</p>
+					<img id="img-competence" src="/img/java.png">
+				</div>
+
+				<div id="compet3">
+					<p>php</p>
+					<img id="img-competence" src="/img/php.png">
+				</div>
+
+				<div id="compet4">
+					<p>html</p>
+					<img id="img-competence" src="/img/html.png">
+				</div>
+
+				<div id="compet5">
+					<p>css</p>
+					<img id="img-competence" src="/img/css.png">
+				</div>
+
+
+			</div>
+
+			<div id="compet6">
+				<p id="text-os-l">linux</p>
+				<img id="img-competence" src="/img/linux.png">
+			</div>
+
+			<div id="compet7">
+				<p id="text-os-w">windows</p>
+				<img id="img-competence" src="/img/windows.png">
+			</div>
+
+			<img id="presentation-img" src="/img/t2.jpg">
 
 		</div>	
 			
-		<div id="programmation">
-			<h1>programmation</h1>		
+
+
+
+
+		<div id="projet">
+
+			<div id="projet-text">
+
+				<h1 id="projet-title"><u>Projet(s)</u></h1>		
+
+			</div>
+
+			<div id="projet-explain">
+				
+			</div>	
+
+			<img id="projet-img" src="/img/t3.jpg">
+
 		</div>
 	</div>
 </body>
